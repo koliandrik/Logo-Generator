fb84cf76239481b7494b816f4ee4983a793902de
koliandrik <koliandrik@gmail.com>
2024-06-18 05:57:34 -0700

    created test

--- index.js
+++ index.js
@@ -24,11 +24,37 @@ const questions = [
         type: 'input',
         name: 'shapeColor',
         message: 'Enter a shape color:',
-    },
-    {
-        type: 'input',
-        name: 'border',
-        message: 'Enter a border width:',
-    },
+    }
 ];
 
+(async () => {
+    const inquirer = await import('inquirer');
+
+    inquirer.default.prompt(questions).then(answers => {
+        const { name, shape, textColor, shapeColor } = answers;
+
+        let shapeObj;
+        switch (shape) {
+            case 'circle':
+                shapeObj = new Circle(shapeColor);
+                break;
+            case 'square':
+                shapeObj = new Square(shapeColor);
+                break;
+            case 'triangle':
+                shapeObj = new Triangle(shapeColor);
+                break;
+            case 'diamond':
+                shapeObj = new Diamond(shapeColor);
+                break;
+        }
+        const svg = `
+            <svg width="300" height="200">
+                ${shapeObj.render()}
+                <text x="150" y="100" font-size="50" fill="${textColor}" text-anchor="middle">${name}</text>
+            </svg>
+        `;
+        fs.writeFileSync( './examples/logo.svg', svg);
+    }
+    );
+})();
